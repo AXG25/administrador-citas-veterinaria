e5721773efc2a5a0b242e662ef64ef385225d968
AXG25 <axiever.guejia@gmail.com>
2023-01-04 13:44:14 -0500

    Primer Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header (){
+
+    return(
+        <header>
+            <h1 className="font-black text-5xl text-center">Seguimientos Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+        </header>
+        
+    )
+}
+
+export default Header
